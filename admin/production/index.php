62b796f195603d3b2a632e4670bea406bfd0f54b
Sedat Bilece <seda1tbilece@gmail.com>
2021-07-12 22:47:04 +0300

    admin paneli eklenmesi

---
+++ admin/production/index.php
@@ -0,0 +1,22 @@
+<?php  include "header.php"; ?>
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+          
+
+            <div class="clearfix"></div>
+
+              
+
+            
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+      <?php 
+      
+      include "footer.php";
+      
+      ?>
