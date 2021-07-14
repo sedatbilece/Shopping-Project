cf80a933145827f8483b4a7d19e64a651b212e39
Sedat Bilece <seda1tbilece@gmail.com>
2021-07-14 15:14:16 +0300

    kullanici ön yüz

--- admin/production/header.php
+++ admin/production/header.php
@@ -120,13 +120,13 @@ echo $kulcek["kullanici_ad"];
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <h3>General</h3>
-                <ul class="nav side-menu">
 
                 
+                <ul class="nav side-menu">
+
                 <li><a href="#"><i class="fa fa-home"></i> AnaSayfa  </li>
                 <li><a href="hakkimizda.php"><i class="fa fa-info"></i> Hakkımızda  </li>
-
-
+                <li><a href="kullanici.php"><i class="fa fa-user"></i> Kullanıcı  </li>
 
                   <li><a><i class="fa fa-cogs"></i> Ayarlar <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
---
+++ admin/production/kullanici.php
@@ -0,0 +1,83 @@
+<?php 
+include "header.php";
+
+
+
+
+$kullanicisor=$db->prepare("SELECT * FROM kullanici ");
+
+$kullanicisor->execute();
+
+
+
+
+?>
+
+
+
+ <!-- page content -->
+ <div class="right_col" role="main">
+          <div class="">
+          
+
+            <div class="clearfix"></div>
+
+              
+
+         
+
+<table class="table table-bordered">
+  <thead class="thead-light">
+    <tr>
+      
+      <th scope="col">Tarih</th>
+      <th scope="col">Ad</th>
+      <th scope="col">Mail</th>
+      <th scope="col">Yetki</th>
+      <th scope="col">Durum</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+
+  <tbody>
+
+
+  <?php while($kulcek= $kullanicisor->fetch(PDO::FETCH_ASSOC) )
+  
+  { ?>
+
+
+<tr>
+     
+      <td> <?php echo $kulcek["kullanici_zaman"];?> </td>
+      <td><?php echo $kulcek["kullanici_ad"];?> </td>
+      <td><?php echo $kulcek["kullanici_mail"];?> </td>
+      <td><?php echo $kulcek["kullanici_yetki"];?> </td>
+      <td><?php echo $kulcek["kullanici_durum"];?> </td>
+     
+      <td><Button class="btn btn-primary btn-xs">Düzenle</Button></td>
+      <td><Button class="btn btn-danger btn-xs">Sil</Button></td>
+      
+    </tr>
+
+  <?php } ?>
+ 
+  
+   
+  </tbody>
+</table>
+
+            
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+
+
+
+
+
+
+<?php include "footer.php"; ?>
